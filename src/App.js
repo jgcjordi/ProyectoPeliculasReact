31a4e031da39121872e8b92c46b23632be0d3973
Jordi Gomis <jgc.jordi@gmail.com>
2019-10-26 13:58:31 +0200

    Añadido HashRouter para funcionamiento correcto con github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import MoviesCategory from './views/MoviesCategory';
 import MovieDetail from './views/MovieDetail';
 import NotFound from './views/NotFound';
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
 
-      <BrowserRouter>
+      <HashRouter basename='/'>
         <NavigationBar />
         <Switch>
           <Route path="/category/:categoryName" exact component={MoviesCategory}/>
@@ -23,7 +23,7 @@ function App() {
           <Redirect path='/' to='category/popular' />
           <Route component={NotFound} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
